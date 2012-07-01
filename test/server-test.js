8bf0f22714d1fc25d2fcaa760e363959a2c02c71
Jared Hanson <jaredhanson@gmail.com>
2012-06-30 21:14:09 -0700

    Basic Server test cases.

---
+++ test/server-test.js
@@ -0,0 +1,36 @@
+var vows = require('vows');
+var assert = require('assert');
+var util = require('util');
+var Server = require('server');
+
+
+vows.describe('Server').addBatch({
+  
+  'Server': {
+    topic: function() {
+      return new Server();
+    },
+    
+    'should wrap requestToken middleware': function (server) {
+      assert.isFunction(server.requestToken);
+      assert.lengthOf(server.requestToken, 3);
+    },
+    'should wrap accessToken middleware': function (server) {
+      assert.isFunction(server.accessToken);
+      assert.lengthOf(server.accessToken, 3);
+    },
+    'should wrap userAuthorization middleware': function (server) {
+      assert.isFunction(server.userAuthorization);
+      assert.lengthOf(server.userAuthorization, 3);
+    },
+    'should wrap userDecision middleware': function (server) {
+      assert.isFunction(server.userDecision);
+      assert.lengthOf(server.userDecision, 3);
+    },
+    'should wrap errorHandler middleware': function (server) {
+      assert.isFunction(server.errorHandler);
+      assert.lengthOf(server.errorHandler, 1);
+    },
+  },
+  
+}).export(module);
